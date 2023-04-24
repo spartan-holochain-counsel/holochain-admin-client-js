ab68082f03b53e9c5baadf5a87cfa09e91063f94
Matthew Brisebois <matthew@webheroes.ca>
2023-04-24 14:28:48 -0600

    Fix node entry additions

--- src/node.js
+++ src/node.js
@@ -1,16 +1,20 @@
 
 import crypto				from 'crypto';
 
-global.crypto				= {
-    "subtle": {
-	digest ( _, bytes ) {
-	    const hash			= crypto.createHash('sha512')
-	    hash.update( bytes );
-	    const digest		= hash.digest();
-	    return digest;
-	},
-    },
-};
+if ( global.crypto === undefined )
+    global.crypto			= {};
+
+if ( global.crypto.subtle?.digest === undefined ) {
+    if ( global.crypto.subtle === undefined )
+	global.crypto.subtle		= {};
+
+    global.crypto.subtle.digest		= ( _, bytes ) => {
+	const hash			= crypto.createHash('sha512');
+	hash.update( bytes );
+	const digest			= hash.digest();
+	return digest;
+    };
+}
 
 import DefaultExports			from './index.js';
 
